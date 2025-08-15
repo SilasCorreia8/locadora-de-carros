a46aa3a0449d7ed1e119419e63d0081c8b93c662
silascorreia <silas8_silva@hotmail.com>
2025-08-14 22:44:48 -0300

    build: /components, /pages

---
+++ locadora-de-carros/src/mockData.js
@@ -0,0 +1,23 @@
+export const cars = [
+  {
+    id: 1,
+    name: 'Fiat Mobi',
+    type: 'Econômico',
+    pricePerDay: 95.50,
+    imageUrl: 'https://i.imgur.com/4612y6b.png'
+  },
+  {
+    id: 2,
+    name: 'Hyundai HB20',
+    type: 'Econômico com A/C',
+    pricePerDay: 115.00,
+    imageUrl: 'https://i.imgur.com/SAX2b4G.png'
+  },
+  {
+    id: 3,
+    name: 'Jeep Renegade',
+    type: 'SUV',
+    pricePerDay: 250.80,
+    imageUrl: 'https://i.imgur.com/So5LAc3.png'
+  },
+];
